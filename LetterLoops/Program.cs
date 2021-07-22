3895fcb0271fdbc218e58a4af68134fe1c9b6fce
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-07-21 19:33:19 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            var letters = Console.ReadLine();
+            char upperLetter;
+            var counter = 1;
+            var increment = 0;
+            var index = 0;
+            var letterList = new List<string>();
+
+            for (var i = 0; i < letters.Length; i++)
+            {
+                upperLetter = Char.ToUpper(letters[i]);
+                letterList.Add(upperLetter.ToString());
+                counter++;
+            }
+
+            for (var j = 0; j < letters.Length; j++)
+            {
+                while (index < increment) // increment index but then reset to 0;
+                {
+                    letterList[j] += letterList[j][0].ToString().ToLower();
+                    index++;
+                }
+                index = 0;
+                increment++;
+                letterList[j] += "-";
+            }
+            string result = string.Join("", letterList);
+            Console.WriteLine(result.TrimEnd('-'));
+        }
+    }
+}
